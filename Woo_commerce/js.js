c24421dac198c3bbbe0c86f6be772aad5d3a72e2
Loi17072001 <tranhuynhloi303@gmail.com>
2022-07-15 15:18:09 +0700

    fix

--- Woo_commerce/js.js
+++ Woo_commerce/js.js
@@ -51,6 +51,29 @@ ScrollReveal().reveal('.footer_bottom_content .footer_bottom_content_list', { de
 
 
 
+const body = document.body;
+let lastScroll = 0;
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset
+  
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up")
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+   body.classList.remove("scroll-up")
+   body.classList.add("scroll-down")
+   
+  }
+
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-down")
+    body.classList.add("scroll-up")
+    
+   }
+  lastScroll = currentScroll;
+  
+})
 
 
 
